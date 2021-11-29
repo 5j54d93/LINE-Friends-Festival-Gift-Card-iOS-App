2d148d949716c40cee7a8478585af8fc7bd2b813
Ricky Chuang <songsongyyohya@gmail.com>
2021-11-29 22:17:23 +0800

    Initial Commit

---
+++ LINE Festival Card/LINE_Festival_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LINE_Festival_CardApp.swift
+//  LINE Festival Card
+//
+//  Created by 莊智凱 on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct LINE_Festival_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
