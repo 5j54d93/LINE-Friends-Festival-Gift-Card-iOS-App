55009c28542b9bd41c453a383106328d1488541d
Ricky Chuang <songsongyyohya@gmail.com>
2022-03-06 15:17:10 +0800

    Create LINE_Festival_CardTests.swift

---
+++ Tests/LINE-Festival-CardTests/LINE_Festival_CardTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LINE_Festival_Card
+
+final class LINE_Festival_CardTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LINE_Festival_Card().text, "Hello, World!")
+    }
+}
